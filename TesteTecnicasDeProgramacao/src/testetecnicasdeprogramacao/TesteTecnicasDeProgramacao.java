cff8e73931694d71295f6256555f5c8f2736d3b9
Leandro Luis <leandroluis1226@gmail.com>
2025-08-22 20:46:08 -0300

    Inserido texto no projeto

--- TesteTecnicasDeProgramacao/src/testetecnicasdeprogramacao/TesteTecnicasDeProgramacao.java
+++ TesteTecnicasDeProgramacao/src/testetecnicasdeprogramacao/TesteTecnicasDeProgramacao.java
@@ -14,7 +14,7 @@ public class TesteTecnicasDeProgramacao {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Testando o projeto do GitHub");
     }
     
 }
